6b7928cf25b716a8eefd268a7e1a9a519df9c60b
Nikola <nikolamarin@protonmail.com>
2019-07-15 23:00:50 +0200

    Login Form

---
+++ Demo/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Demo.Models;
+
+namespace Demo.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogIn(UserModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Demo/Models/UserModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Demo.Models
+{
+    public class UserModel
+    {
+        [Display(Name = "Username")]
+        [Required(ErrorMessage = "Username is required for logging in!")]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "Minimum username length is 4 and maximum is 20")]
+        public string UserName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Passowrd is required!")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "Wrong! Minimum password length is 4 and maximum 100")]  
+        public string Password { get; set; }
+        public List<string> Notes { get; set; }
+
+    }
+}
